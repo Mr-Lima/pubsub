36c8b31e4bf9098c6e12dacba32e4e8a45a1be8f
Andre <andre.limagg@hotmail.com>
2019-03-01 00:25:28 -0300

    handling messages

--- src/main/java/aldev/PubSub.java
+++ src/main/java/aldev/PubSub.java
@@ -3,6 +3,7 @@ package aldev;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
@@ -35,5 +36,37 @@ public class PubSub {
 
     Set<AbstractSubscriber> subscribers = topicSubscriberMap.get(topic);
     subscribers.remove(subscriber);
+  }
+
+  public void getMessageForSubscriber(String topic, AbstractSubscriber subscriber) {
+    Queue<Message> messages = messagesQueue;
+    while (!messages.isEmpty()) {
+      Message message = messages.remove();
+
+      if (message.getTopic().equalsIgnoreCase(topic)) {
+        Set<AbstractSubscriber> subscribers = topicSubscriberMap.get(topic);
+        subscribers.forEach(_subscriber -> {
+          if (_subscriber.equals(subscriber)) {
+            List<Message> subscriberMessages = _subscriber.getSubscriberMessages();
+            subscriberMessages.add(message);
+            _subscriber.setSubscriberMessages(subscriberMessages);
+          }
+        });
+      }
+    }
+  }
+
+  public void broadcast() {
+    while (!this.messagesQueue.isEmpty()) {
+      Message message = messagesQueue.remove();
+
+      Set<AbstractSubscriber> subscribers = topicSubscriberMap.get(message.getTopic());
+
+      subscribers.forEach(subscriber -> {
+        List<Message> messages = subscriber.getSubscriberMessages();
+        messages.add(message);
+        subscriber.setSubscriberMessages(messages);
+      });
+    }
   }
 }
